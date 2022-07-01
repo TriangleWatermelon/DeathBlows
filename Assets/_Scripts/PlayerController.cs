9bd376856e35bb27110ff68c8562f36272ac24bd
AlecCDAS <102942141+AlecCDAS@users.noreply.github.com>
2022-06-30 21:07:22 -0500

    Player movement setup

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,111 +1,144 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 using Sirenix.OdinInspector;
-using System;
 
 public class PlayerController : MonoBehaviour
 {
-    Camera playerCam;
-    [BoxGroup("Camera")]
-    [SerializeField] Vector3 cameraOffset;
     #region Visuals
     [TitleGroup("Main")]
-    [HorizontalGroup("Main")]
-    [VerticalGroup("Main")]
     [BoxGroup("Main/Visuals")]
     [PreviewField(70, ObjectFieldAlignment.Left)]
     [SerializeField] Sprite playerSprite;
     SpriteRenderer spriteRenderer;
-    bool isFacingRight = true;
     #endregion
+
     #region STATS
-    [VerticalGroup("Main")]
+    [TitleGroup("Main")]
     [BoxGroup("Main/Stats")]
     [SerializeField] float maxHealth;
     private float health;
-    [VerticalGroup("Main")]
+    [TitleGroup("Main")]
     [BoxGroup("Main/Stats")]
     [SerializeField] float moveSpeed;
-    [VerticalGroup("Main")]
+    [TitleGroup("Main")]
     [BoxGroup("Main/Stats")]
     [SerializeField] float jumpHeight;
     #endregion
+
     #region Control
-    Rigidbody2D rb2d;
     PlayerActions playerActions;
     Vector2 moveDir;
-    bool isJumping = false;
     bool isAttacking = false;
     bool isBubbling = false;
     [TitleGroup("Control")]
-    [BoxGroup("Control/Raycasting")]
-    [SerializeField] float rayDistance;
+    [BoxGroup("Control/Movement")]
+    [SerializeField] float movementSmoothing;
+    Vector3 velocity = Vector3.zero;
+    bool isGrounded;
+    [TitleGroup("Control")]
+    [BoxGroup("Control/Movement")]
+    [SerializeField] Transform groundCheck;
+    const float groundCheckRadius = 0.2f;
+    [TitleGroup("Control")]
+    [BoxGroup("Control/Movement")]
+    [SerializeField] LayerMask groundLayer;
+    Rigidbody2D rb2d;
+    bool isFacingRight = true;
     #endregion
+
+    public UnityEvent OnLandEvent;
 
     private void Awake()
     {
-        playerCam = FindObjectOfType<Camera>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sprite = playerSprite;
+
         playerActions = new PlayerActions();
+
         rb2d = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
     }
 
     void Start()
     {
         health = maxHealth;
 
-        playerActions.Gameplay.Move.performed += ctx => moveDir = ctx.ReadValue<Vector2>();
-        playerActions.Gameplay.Move.performed += ctx => OnMove();
-        playerActions.Gameplay.Move.canceled += ctx => moveDir = -moveDir;
-        playerActions.Gameplay.Move.canceled += ctx => OnMove();
         playerActions.Gameplay.Jump.performed += ctx => OnJump();
     }
 
-    void OnMove()
+    private void Update()
     {
-        moveDir.y = 0;
-        rb2d.AddForce(moveDir * moveSpeed);
+        moveDir = playerActions.Gameplay.Move.ReadValue<Vector2>();
+        Move(moveDir, moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated on the ground layer
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundCheckRadius, groundLayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
     }
 
     void OnJump()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, Vector2.down, out hit, rayDistance))
+        Jump(jumpHeight);
+    }
+
+    public void Move(Vector2 moveDir, float moveSpeed)
+    {
+        if (moveDir.x < 0 && isFacingRight)
         {
-            Debug.Log("Hit!");
+            FlipSprite();
         }
-        if (!isJumping)
+        if (moveDir.x > 0 && !isFacingRight)
         {
-            rb2d.AddForce(Vector2.up * jumpHeight);
+            FlipSprite();
+        }
+        if (Mathf.Abs(moveDir.x) < 0.2f)
+        {
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = Vector2.zero + new Vector2(0, rb2d.velocity.y);
+            // And then smoothing it out and applying it to the character
+            rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, 0.1f);
+        }
+        else
+        {
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2((moveDir.x * moveSpeed), rb2d.velocity.y);
+            // And then smoothing it out and applying it to the character
+            rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, movementSmoothing);
         }
     }
 
-    void Update()
+    public void Jump(float jumpForce)
     {
-        playerCam.transform.position = transform.position + cameraOffset;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        string colTag = collision.gameObject.tag;
-        switch (colTag)
+        if (isGrounded)
         {
-            case "Ground":
-                isJumping = false;
-                break;
+            isGrounded = false;
+            rb2d.AddForce(new Vector2(0f, jumpForce * 100));
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    void FlipSprite()
     {
-        string colTag = collision.gameObject.tag;
-        switch (colTag)
-        {
-            case "Ground":
-                isJumping = true;
-                break;
-        }
+        isFacingRight = !isFacingRight;
+
+        Vector3 flipScale = transform.localScale;
+        flipScale.x *= -1;
+        transform.localScale = flipScale;
     }
 
     private void OnEnable()
